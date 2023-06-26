0651c72f88656b0ae0afbbd2768c15ad8c72288e
İsmail Harmanda <iharmanda@hotmail.com>
2023-06-27 01:53:37 +0300

    render posts in List via Identifiable protocol

--- Hacker News/ContentView.swift
+++ Hacker News/ContentView.swift
@@ -11,9 +11,8 @@ struct ContentView: View {
     var body: some View {
         VStack {
             NavigationView {
-                List {
-                    Text("Hello, world!")
-                    Text("Good bye, world!")
+                List(posts) { post in
+                    Text(post.title)
                 }
                 .navigationTitle("HACKER NEWS")
             }
@@ -31,3 +30,12 @@ struct Post: Identifiable {
     let id: String
     let title: String
 }
+
+
+
+let posts = [
+    Post(id:"1",title:"Hello"),
+    Post(id:"2",title:"Bonjout"),
+    Post(id:"3",title:"Hola"),
+    Post(id:"4",title:"Merhaba"),
+]
