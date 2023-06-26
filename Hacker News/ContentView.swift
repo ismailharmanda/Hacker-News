bd4c72073bae7f20f92df65da3e2aceb7a9be346
İsmail Harmanda <iharmanda@hotmail.com>
2023-06-27 01:35:56 +0300

    add a list view inside navigation and set navigation title

--- Hacker News/ContentView.swift
+++ Hacker News/ContentView.swift
@@ -10,12 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            NavigationView {
+                List {
+                    Text("Hello, world!")
+                    Text("Good bye, world!")
+                }
+                .navigationTitle("HACKER NEWS")
+            }
         }
-        .padding()
     }
 }
 
