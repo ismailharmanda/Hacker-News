7f396cf5910ff5553b2e006133b80d553e5cd25b
İsmail Harmanda <iharmanda@hotmail.com>
2023-06-27 01:54:09 +0300

    fix typo

--- Hacker News/ContentView.swift
+++ Hacker News/ContentView.swift
@@ -35,7 +35,7 @@ struct Post: Identifiable {
 
 let posts = [
     Post(id:"1",title:"Hello"),
-    Post(id:"2",title:"Bonjout"),
+    Post(id:"2",title:"Bonjour"),
     Post(id:"3",title:"Hola"),
     Post(id:"4",title:"Merhaba"),
 ]
