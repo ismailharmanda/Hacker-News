ed918cadf5b0a76b86df384a2bc8284867314fbd
İsmail Harmanda <iharmanda@hotmail.com>
2023-06-27 11:16:42 +0300

    create Network Manager model

---
+++ Hacker News/Models/NetworkManager.swift
@@ -0,0 +1,23 @@
+//
+//  NetworkManager.swift
+//  Hacker News
+//
+//  Created by ismail harmanda on 27.06.2023.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+    func fetchData() {
+        if let url = URL(string: "http://hn.algolia.com/api/v1/search?tags=front_page") {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { data, response, error in
+                if error == nil {
+                    let decoder = JSONDecoder()
+                    
+                }
+            }
+        }
+    }
+}
