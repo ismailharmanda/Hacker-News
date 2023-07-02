a13878a36c3920c82d815a69b9e933a4157612b9
İsmail Harmanda <iharmanda@hotmail.com>
2023-07-02 21:51:39 +0300

    wrap posts via NaviationLink and navigate to DetailView on press

--- Hacker News/Views/ContentView.swift
+++ Hacker News/Views/ContentView.swift
@@ -13,18 +13,21 @@ struct ContentView: View {
     
     @ObservedObject var networkManager = NetworkManager()
     
-
     
-        
+    
+    
     
     var body: some View {
         VStack {
             NavigationView {
                 List(networkManager.posts) { post in
-                    HStack {
-                        Text(String(post.points))
-                        Text(post.title)
+                    NavigationLink(destination: DetailView(url: post.url)) {
+                        HStack {
+                            Text(String(post.points))
+                            Text(post.title)
+                        }
                     }
+                   
                 }
                 .navigationTitle("HACKER NEWS")
             }
