09042058cf7488939a8a3efce809f36ba31d6332
İsmail Harmanda <iharmanda@hotmail.com>
2023-07-02 22:18:13 +0300

    add a webview via WebKit

--- Hacker News/Views/DetailView.swift
+++ Hacker News/Views/DetailView.swift
@@ -6,18 +6,37 @@
 //
 
 import SwiftUI
+import WebKit
 
 struct DetailView: View {
     
     let url: String?
     
     var body: some View {
-        Text("Hello, World!")
+        WebView(urlString: url)
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
         DetailView(url: "https://www.google.com")
+    }
+}
+
+struct WebView: UIViewRepresentable {
+    
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeUrlString = urlString {
+            if let url = URL(string: safeUrlString){
+                let request = URLRequest(url: url)
+                uiView.load(request)}
+        }
+        
     }
 }
