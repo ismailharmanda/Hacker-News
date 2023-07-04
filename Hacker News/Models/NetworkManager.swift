681eb07acbc22e0c4caba1e5fbfcffe056615e11
İsmail Harmanda <iharmanda@hotmail.com>
2023-07-04 11:16:15 +0300

    add Mac support and handle question data causes the crash because there is no url information

--- Hacker News/Models/NetworkManager.swift
+++ Hacker News/Models/NetworkManager.swift
@@ -19,11 +19,12 @@ class NetworkManager: ObservableObject {
             
             let task = session.dataTask(with: url) { data, response, error in
                 if error == nil {
+                    
                     let decoder = JSONDecoder()
                     if let safeData = data {
                         
                         do {
-                          let results = try decoder.decode(Result.self, from:safeData)
+                            let results = try decoder.decode(Result.self, from:safeData)
                             DispatchQueue.main.async {
                                 self.posts = results.hits
                             }
